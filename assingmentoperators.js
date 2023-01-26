c634a545e8c9184bade1902c1c7a3b9d1990fcc3
PabaMax404 <pabamaxwithcomputers@gmail.com>
2023-01-26 03:05:26 -0800

    initial commit

---
+++ assingmentoperators.js
@@ -0,0 +1,21 @@
+//equal assingmentoperators
+var myName = "pabasara";
+console.log (myName);
+
+var age = 24;
+age += 10;
+console.log('age');
+
+//subraction with equal
+age -= 10;
+console.log('age');
+
+//multiply with equal
+age *= 10;
+console.log('age');
+
+//division with equal
+age *= 10;
+console.log('age');
+
+//
